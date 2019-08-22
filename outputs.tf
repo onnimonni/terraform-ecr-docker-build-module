eadc1ed4cdae762d5d4564a5437eb30f77ff85d1
RodneyPerez <Rodney.the.programmer@gmail.com>
2019-08-21 21:45:38 -0400

    Ran terraform 0.12upgrade command on files

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "ecr_image_url" {
-  value = "${var.ecr_repository_url}:${var.docker_image_tag}"
+  value       = "${var.ecr_repository_url}:${var.docker_image_tag}"
   description = "Full URL to image in ecr with tag"
 }
+
--- variables.tf
+++ variables.tf
@@ -1,21 +1,22 @@
 variable "dockerfile_folder" {
-  type = "string"
+  type        = string
   description = "This is the folder which contains the Dockerfile"
 }
 
 variable "docker_image_tag" {
-  type = "string"
+  type        = string
   description = "This is the tag which will be used for the image that you created"
-  default = "latest"
+  default     = "latest"
 }
 
 variable "aws_region" {
-  type = "string"
+  type        = string
   description = "AWS region for ECR"
-  default = ""
+  default     = ""
 }
 
 variable "ecr_repository_url" {
-  type = "string"
+  type        = string
   description = "Full url for the ecr repository"
 }
+
