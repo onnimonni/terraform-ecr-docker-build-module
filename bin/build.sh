cbb2c5a2d569a2ef6f98d92f19b66da8689500ae
Yuval Schnitzer <yuval@seemplicity.io>
2020-12-29 12:46:14 +0200

    now gettings the tag and the repo name plus pushing the image with an optional extra tag

--- bin/build.sh
+++ bin/build.sh
@@ -5,13 +5,14 @@ set -e
 
 # This is the order of arguments
 build_folder=$1
-aws_ecr_repository_url_with_tag=$2
+aws_ecr_repository_url=$2
+tag=$3
 # kept for backwards compatibility
 
-ecs_cluster_name=$3
-ecs_service_name=$4
-additional_docker_tag=$5
-aws_region=$6
+ecs_cluster_name=$4
+ecs_service_name=$5
+additional_docker_tag=$6
+aws_region=$7
 
 # Allow overriding the aws region from system
 if [ "$aws_region" != "" ]; then
@@ -26,15 +27,18 @@ which aws > /dev/null || { echo 'ERROR: aws-cli is not installed' ; exit 1; }
 which docker > /dev/null && docker ps > /dev/null || { echo 'ERROR: docker is not running' ; exit 1; }
 
 # Connect into aws
-aws ecr get-login-password $aws_extra_flags | docker login --username AWS --password-stdin $aws_ecr_repository_url_with_tag
+aws ecr get-login-password $aws_extra_flags | docker login --username AWS --password-stdin $aws_ecr_repository_url:$tag
 # Some Useful Debug
-echo "Building $aws_ecr_repository_url_with_tag from $build_folder/Dockerfile"
+echo "Building $aws_ecr_repository_url:$tag from $build_folder/Dockerfile"
 
 # Build image
-docker build -t $aws_ecr_repository_url_with_tag $build_folder -t $additional_docker_tag
-
+if [ "$additional_docker_tag" != "" ]; then
+  docker build -t $aws_ecr_repository_url:$tag -t $aws_ecr_repository_url:$additional_docker_tag $build_folder
+else
+  docker build -t $aws_ecr_repository_url:$tag $build_folder
+fi
 # Push image
-docker push $aws_ecr_repository_url_with_tag --all-tags
+docker push $aws_ecr_repository_url --all-tags
 
 # Update the ecs service
 aws ecs update-service --cluster $ecs_cluster_name  --service $ecs_service_name --force-new-deployment
